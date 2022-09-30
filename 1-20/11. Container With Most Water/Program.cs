52533cf86456a52b550694ecb804e2b391134501
Zhanibek Smagul <smagul.zhanibek@gmail.com>
2022-09-30 07:23:39 +0600

    11. Container With Most Water

---
+++ 1-20/11. Container With Most Water/Program.cs
@@ -0,0 +1,32 @@
+﻿// https://leetcode.com/problems/container-with-most-water/
+// Difficulty - #Medium
+// Topics - #Array, #Two_Pointers, #Greedy
+
+int[] height = new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+int maxArea = MaxArea(height);
+Console.WriteLine(maxArea); // 49
+
+// Time complexity  : O(n)
+// Space complexity : O(1)
+int MaxArea(int[] height)
+{
+    int maxArea = 0;
+    int left = 0;
+    int right = height.Length - 1;
+    while (left < right)
+    {
+        int currArea = right - left;
+        if (height[left] < height[right])
+        {
+            currArea *= height[left++];
+        }
+        else
+        {
+            currArea *= height[right--];
+        }
+
+        maxArea = Math.Max(maxArea, currArea);
+    }
+
+    return maxArea;
+}
