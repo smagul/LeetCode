69b36c8127f6ab059ed39702b8f18e8f3e817799
Zhanibek Smagul <smagul.zhanibek@gmail.com>
2022-10-02 12:23:05 +0600

    567. Permutation in String

--- 561-580/567. Permutation in String/Program.cs
+++ 561-580/567. Permutation in String/Program.cs
@@ -10,5 +10,46 @@ Console.WriteLine(included); // true
 // Space complexity : O(1)
 bool CheckInclusion(string s1, string s2)
 {
+    if (s1.Length > s2.Length)
+    {
+        return false;
+    }
+
+    int[] arr = new int[26];
+    for (int k = 0; k < s1.Length; k++)
+    {
+        arr[s1[k] - 'a']++;
+        arr[s2[k] - 'a']--;
+    }
+
+    if (IsEmpty(arr))
+    {
+        return true;
+    }
+
+    int i = 0, j = s1.Length;
+    while (j < s2.Length)
+    {
+        if (IsEmpty(arr))
+        {
+            return true;
+        }
+        arr[s2[i++] - 'a']++;
+        arr[s2[j++] - 'a']--;
+    }
+
     return false;
+}
+
+bool IsEmpty(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] != 0)
+        {
+            return false;
+        }
+    }
+
+    return true;
 }
