78e665c724be197a2cbeccc5e198f7af423bbbac
Zhanibek Smagul <smagul.zhanibek@gmail.com>
2022-10-02 12:03:52 +0600

    567. Permutation in String

---
+++ 561-580/567. Permutation in String/Program.cs
@@ -0,0 +1,14 @@
+﻿// https://leetcode.com/problems/permutation-in-string/
+// Difficulty - #Medium
+// Topics - #Hash_Table, #Two_Pointers, #String, #Sliding_Window
+
+string s1 = "ab", s2 = "eidbaooo";
+bool included = CheckInclusion(s1, s2);
+Console.WriteLine(included); // true
+
+// Time complexity  : O(n)
+// Space complexity : O(1)
+bool CheckInclusion(string s1, string s2)
+{
+    return false;
+}
