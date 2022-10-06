22fd11629d0d16a71bc7f0c12f637970488bfe9f
Zhanibek Smagul <smagul.zhanibek@gmail.com>
2022-10-06 08:12:03 +0600

    150. Evaluate Reverse Polish Notation

---
+++ 141-160/150. Evaluate Reverse Polish Notation/Program.cs
@@ -0,0 +1,38 @@
+﻿// https://leetcode.com/problems/evaluate-reverse-polish-notation/
+// Difficulty - #Medium
+// Topics - #Array, #Math, #Stack
+
+string[] tokens = new string[] { "2", "1", "+", "3", "*" };
+int result = EvalRPN(tokens);
+Console.WriteLine(result); // 9
+
+int EvalRPN(string[] tokens)
+{
+    Stack<int> stack = new();
+
+    foreach (string token in tokens)
+    {
+        switch (token)
+        {
+            case "+":
+                stack.Push(stack.Pop() + stack.Pop());
+                break;
+            case "-":
+                int sub = stack.Pop();
+                stack.Push(stack.Pop() - sub);
+                break;
+            case "*":
+                stack.Push(stack.Pop() * stack.Pop());
+                break;
+            case "/":
+                int div = stack.Pop();
+                stack.Push(stack.Pop() / div);
+                break;
+            default:
+                stack.Push(Convert.ToInt32(token));
+                break;
+        }
+    }
+
+    return stack.ElementAt(0);
+}
