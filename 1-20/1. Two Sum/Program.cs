768f6e3123e4b425d7b42f4a62dbbfc26381b34f
Zhanibek Smagul <smagul.zhanibek@gmail.com>
2022-10-14 09:08:51 +0600

    1. Two Sum 853. Car Fleet

---
+++ 1-20/1. Two Sum/Program.cs
@@ -0,0 +1,33 @@
+﻿// https://leetcode.com/problems/two-sum/
+// Difficulty - #Easy
+// Topics - #Array, #Hash_Table
+
+int target = 9;
+int[] nums = new int[] { 2, 7, 11, 15 };
+int[] sums = TwoSum(nums, target);
+foreach (int sum in sums)
+{
+    Console.WriteLine(sum); // 0, 1
+}
+
+// Time complexity  : O(n)
+// Space complexity : O(n)
+int[] TwoSum(int[] nums, int target)
+{
+    Dictionary<int, int> dict = new();
+    for (int i = 0; i < nums.Length; i++)
+    {
+        int diff = target - nums[i];
+        if (dict.ContainsKey(diff))
+        {
+            return new int[] { dict[diff], i };
+        }
+
+        if (!dict.ContainsKey(nums[i]))
+        {
+            dict.Add(nums[i], i);
+        }
+    }
+
+    return new int[] { };
+}
---
+++ 841-860/853. Car Fleet/Program.cs
@@ -0,0 +1,41 @@
+﻿// https://leetcode.com/problems/car-fleet/
+// Difficulty - #Medium
+// Topics - #Array, #Stack, #Sorting, #Monotonic_Stack
+
+int target = 12;
+int[] position = new int[] { 10, 8, 0, 5, 3 };
+int[] speed = new int[] { 2, 4, 1, 1, 3 };
+int numberOfFleets = CarFleet(target, position, speed);
+Console.Write(numberOfFleets); // 3
+
+// Time complexity  : O(nLogn)
+// Space complexity : O(n)
+int CarFleet(int target, int[] position, int[] speed)
+{
+    if (position.Length == 1)
+    {
+        return 1;
+    }
+
+    Stack<double> stack = new();
+    int[][] combine = new int[position.Length][];
+    for (int i = 0; i < position.Length; i++)
+    {
+        combine[i] = new int[2] { position[i], speed[i] };
+    }
+
+    Array.Sort(combine, (x, y) => x[0].CompareTo(y[0]));
+
+    for (int i = combine.Length - 1; i >= 0; i--)
+    {
+        double currentTime = (double)(target - combine[i][0] / combine[i][1]);
+        if (stack.Count > 0 && stack.Peek() >= currentTime)
+        {
+            continue;
+        }
+
+        stack.Push(currentTime);
+    }
+
+    return stack.Count;
+}
