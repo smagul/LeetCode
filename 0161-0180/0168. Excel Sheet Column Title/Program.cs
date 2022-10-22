9bef2bf081fef639e65726e537a004ca5206f416
Zhanibek Smagul <smagul.zhanibek@gmail.com>
2022-10-22 07:28:43 +0600

    168. Excel Sheet Column Title

---
+++ 0161-0180/0168. Excel Sheet Column Title/Program.cs
@@ -0,0 +1,16 @@
+﻿// https://leetcode.com/problems/excel-sheet-column-title/
+// Difficulty - #Easy
+// Topics - #Math, #String
+
+int columnNumber = 701;
+string result = ConvertToTitle(columnNumber);
+Console.WriteLine(result); // ZY
+
+// Time complexity  : O(n)
+// Space complexity : O(1)
+string ConvertToTitle(int columnNumber)
+{
+    return columnNumber == 0
+        ? string.Empty
+        : ConvertToTitle((columnNumber - 1) / 26) + (char)('A' + (columnNumber - 1) % 26);
+}
