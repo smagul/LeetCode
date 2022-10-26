97527ccfa4ef4f3e851b4f6bb36ddf42e5807953
Zhanibek Smagul <smagul.zhanibek@gmail.com>
2022-10-26 22:54:20 +0600

    153. Find Minimum in Rotated Sorted Array

---
+++ 0141-0160/0153. Find Minimum in Rotated Sorted Array/Program.cs
@@ -0,0 +1,30 @@
+﻿// https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+// Difficulty - #Medium
+// Topics - #Array, #Binary_Search
+
+int[] nums = new int[] { 4, 5, 6, 7, 0, 1, 2 };
+int min = FindMin(nums);
+Console.WriteLine(min);
+
+// Time complexity  : O(logn)
+// Space complexity : O(1)
+int FindMin(int[] nums)
+{
+    int l = 0;
+    int r = nums.Length - 1;
+
+    while (l < r)
+    {
+        int m = (l + r) / 2;
+        if (nums[m] < nums[r])
+        {
+            r = m;
+        }
+        else
+        {
+            l = m + 1;
+        }
+    }
+
+    return nums[l];
+}
