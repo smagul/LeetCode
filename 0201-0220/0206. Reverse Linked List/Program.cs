3b9fe87bbeca00abef797e7dd8054f48d6e3a957
Zhanibek Smagul <smagul.zhanibek@gmail.com>
2022-11-02 07:50:30 +0600

    206. Reverse Linked List

---
+++ 0201-0220/0206. Reverse Linked List/Program.cs
@@ -0,0 +1,41 @@
+﻿// https://leetcode.com/problems/reverse-linked-list/
+// Difficulty - #Easy
+// Topics - #Linked_List, #Recursion
+
+var root = ReverseList(new(1, new(2, new(3, new(4, new(5, null))))));
+while (root != null)
+{
+    Console.Write(root.val + " "); // 5,4,3,2,1
+    root = root.next;
+}
+
+// Time complexity  : O(n)
+// Space complexity : O(1)
+ListNode ReverseList(ListNode head)
+{
+    ListNode prev = null;
+
+    while (head != null)
+    {
+        ListNode next = head.next;
+        head.next = prev;
+        prev = head;
+        head = next;
+    }
+
+    return prev;
+}
+
+/**
+ * Definition for singly-linked list.
+ **/
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
